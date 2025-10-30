338a924d6d94da39b46755f68def18f30efc3960
Willian Lipka <willianlipka.dev@proton.me>
2025-10-30 20:12:35 -0300

    atualização seleciona serviço

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const pup = require("puppeteer");
 
 const url = "https://whatsagenda.com.br/barber-shop-carioca";
-const date = "11-9-2025"; // não pode ter 0, erro com tratamento interno e sem acesso no código original
+const date = "4-11-2025"; // não pode ter 0, erro com tratamento interno e sem acesso no código original
 
 //Seletores
+const servicos = ".form-check-input"; //seleciona todos os servicos
 const loadingSelector = ".sk-ball-spin-clockwise.ng-star-inserted"; // Seletor loading da página -> "espiral bolinha de carregando"
 const buttonVejaMais = "#categItemVertical0 button"; //Seletor do botão "VejaMais"
-const cabeloBarba = "#servRadio02"; //Seleciona Cabelo e Barba no site
 const barbeiro = "#prestRadio_1"; //Seleciona o Barbeiro LUKE
 const buttonDate = ".dateSelectionBtn"; //Botão de data
 const selectDate = `[aria-label="${date}"]`; //Seleciona a Data
@@ -32,7 +32,7 @@ const classeBotoesHorarios = ".chooseHorarioBtn"; //Classe que contem todos os b
 
     //Chama a função auxiliar e faz a selação na página
     await waitAndClick(page, buttonVejaMais, "Botão Veja Mais");
-    await waitAndClick(page, cabeloBarba, "Seleção Cabelo e Barba");
+    await selectService(page, servicos, "Seleção Cabelo e Barba");
     await waitAndClick(page, barbeiro, "Seelção do Barbeiro");
     await waitAndClick(page, buttonDate, "Abriu o calendário");
     await waitAndClick(page, selectDate, `Selecionou a Data(${date})`);
@@ -78,3 +78,10 @@ async function waitAndClick(page, selector, logMessage) {
   await page.click(selector);
   console.log(`Ação concluída: ${logMessage}`);
 }
+async function selectService(page, servicos, logMessage) {
+  const seleciona = await page.$$(servicos);
+  await seleciona[2].click();
+  console.log(`Ação concluída: ${logMessage}`);
+}
+
+//estrutura
